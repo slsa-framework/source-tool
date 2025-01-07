33f781197ef321a62035e136e6802641a45a44ab
Tom Hennen <tomhennen@google.com>
2025-01-07 15:06:25 +0000

    add basic, non working, script to determine level 2
    Signed-off-by: Tom Hennen <tomhennen@google.com>

--- actions/vsa_creator/determine_source_level_gh.sh
+++ actions/vsa_creator/determine_source_level_gh.sh
@@ -16,4 +16,17 @@ GITHUB_RULESET=$(curl -L \
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/repos/${REPO}/rules/branches/${BRANCH})
 
-echo $GITHUB_RULESET
+# Check continuity requirement
+# We'll assume it meets this requirement if the branch prevents deletions
+# and force pushes.
+# TODO: Should other things be checked too?
+NO_DELETION=$(echo $GITHUB_RULESET | jq '.[] | select(.type=="deletion")')
+NO_FORCE_PUSH=$(echo $GITHUB_RULESET | jq '.[] | select(.type=="non_fast_forward") | any')
+
+SOURCE_LEVEL="SLSA_SOURCE_LEVEL_1"
+
+# TODO: this isn't working, we'd expect to this to work for `TomHennen/slsa-source-poc main`
+if [ "$NO_DELETION" = "true" ] && [ "$NO_FORCE_PUSH" = "true" ]; then
+    SOURCE_LEVEL="SLSA_SOURCE_LEVEL_2"
+fi
+echo $SOURCE_LEVEL
