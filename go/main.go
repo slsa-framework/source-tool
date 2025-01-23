2eddf3402a168cdb5911571a2fd69ec6ecc7d706
Tom Hennen <tomhennen@google.com>
2025-01-23 18:07:12 +0000

    Improve ruleset checks
    1. Ensure the ruleset is actively enforced.
    2. Ensure it's been active for a user specified number of days.
    
    Signed-off-by: Tom Hennen <tomhennen@google.com>

--- go/main.go
+++ go/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/google/go-github/v68/github"
 )
@@ -14,31 +15,34 @@ const (
 	SlsaSourceLevel2 = "SLSA_SOURCE_LEVEL_2"
 )
 
-// Determines the source level of a repo.
-func main() {
-
-	// TODO: Replace with command line args.
-	var owner, repo, branch string
-	flag.StringVar(&owner, "owner", "", "The GitHub repository owner - required.")
-	flag.StringVar(&repo, "repo", "", "The GitHub repository name - required.")
-	flag.StringVar(&branch, "branch", "", "The branch within the repository - required.")
-	flag.Parse()
-
-	if owner == "" || repo == "" || branch == "" {
-		log.Fatal("Must set owner, repo, and branch flags.")
+// Checks to see if the rule meets our requirements.
+func checkRule(ctx context.Context, gh_client *github.Client, owner string, repo string, rule *github.RepositoryRule, minTime time.Time) (bool, error) {
+	ruleset, _, err := gh_client.Repositories.GetRuleset(ctx, owner, repo, rule.RulesetID, false)
+	if err != nil {
+		return false, err
 	}
 
-	gh_client := github.NewClient(nil)
-	ctx := context.Background()
+	// We need rules to be 'active' and to have been updated no later than minTime.
+	if ruleset.Enforcement != "active" {
+		return false, nil
+	}
+
+	if minTime.Before(ruleset.UpdatedAt.Time) {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func determineSourceLevel(ctx context.Context, gh_client *github.Client, owner string, repo string, branch string, minDays int) (string, error) {
 	rules, _, err := gh_client.Repositories.GetRulesForBranch(ctx, owner, repo, branch)
 
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	var deletionRule *github.RepositoryRule
 	var nonFastFowardRule *github.RepositoryRule
-
 	for _, rule := range rules {
 		switch rule.Type {
 		case "deletion":
@@ -50,9 +54,54 @@ func main() {
 		}
 	}
 
-	sourceLevel := SlsaSourceLevel1
-	if deletionRule != nil && nonFastFowardRule != nil {
-		sourceLevel = SlsaSourceLevel2
+	if deletionRule == nil && nonFastFowardRule == nil {
+		// For L2 we need deletion and non-fast-forward rules.
+		return SlsaSourceLevel1, nil
+	}
+
+	// We'd like to check that the rules have been enabled for the appropriate amount of time
+	// (to ensure they weren't disabled).  For now we just require something that's not in
+	// the future.
+	minTime := time.Now().AddDate(0, 0, -1*minDays)
+
+	deletionGood, err := checkRule(ctx, gh_client, owner, repo, deletionRule, minTime)
+	if err != nil {
+		return "", err
+	}
+	nonFFGood, err := checkRule(ctx, gh_client, owner, repo, nonFastFowardRule, minTime)
+	if err != nil {
+		return "", err
+	}
+
+	if deletionGood && nonFFGood {
+		return SlsaSourceLevel2, nil
+	}
+
+	return SlsaSourceLevel1, nil
+}
+
+// Determines the source level of a repo.
+func main() {
+
+	// TODO: Replace with command line args.
+	var owner, repo, branch string
+	var minDays int
+	flag.StringVar(&owner, "owner", "", "The GitHub repository owner - required.")
+	flag.StringVar(&repo, "repo", "", "The GitHub repository name - required.")
+	flag.StringVar(&branch, "branch", "", "The branch within the repository - required.")
+	flag.IntVar(&minDays, "min_days", 1, "The minimum duration that the rules need to have been enabled for.")
+	flag.Parse()
+
+	if owner == "" || repo == "" || branch == "" {
+		log.Fatal("Must set owner, repo, and branch flags.")
+	}
+
+	gh_client := github.NewClient(nil)
+	ctx := context.Background()
+
+	sourceLevel, err := determineSourceLevel(ctx, gh_client, owner, repo, branch, minDays)
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Print(sourceLevel)
