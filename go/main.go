cc001edf8797e2db4ac11237eabceefd6a533d2c
Tom Hennen <tomhennen@google.com>
2025-01-23 17:32:49 +0000

    add flags
    Signed-off-by: Tom Hennen <tomhennen@google.com>

--- go/main.go
+++ go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 
@@ -15,15 +16,20 @@ const (
 
 // Determines the source level of a repo.
 func main() {
-	gh_client := github.NewClient(nil)
-
-	ctx := context.Background()
 
 	// TODO: Replace with command line args.
-	owner := "TomHennen"
-	repo := "slsa-source-poc"
-	branch := "main"
+	var owner, repo, branch string
+	flag.StringVar(&owner, "owner", "", "The GitHub repository owner - required.")
+	flag.StringVar(&repo, "repo", "", "The GitHub repository name - required.")
+	flag.StringVar(&branch, "branch", "", "The branch within the repository - required.")
+	flag.Parse()
 
+	if owner == "" || repo == "" || branch == "" {
+		log.Fatal("Must set owner, repo, and branch flags.")
+	}
+
+	gh_client := github.NewClient(nil)
+	ctx := context.Background()
 	rules, _, err := gh_client.Repositories.GetRulesForBranch(ctx, owner, repo, branch)
 
 	if err != nil {
