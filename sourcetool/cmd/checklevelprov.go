48b7bea5a62a1a42ed6cdf82f7ec918f4986864b
Tom Hennen <tomhennen@google.com>
2025-02-23 15:49:34 +0000

    fill in command info
    Signed-off-by: Tom Hennen <tomhennen@google.com>

--- sourcetool/cmd/checklevelprov.go
+++ sourcetool/cmd/checklevelprov.go
@@ -34,13 +34,7 @@ var (
 
 	checklevelprovCmd = &cobra.Command{
 		Use:   "checklevelprov",
-		Short: "A brief description of your command",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Short: "Checks the given commit against policy using & creating provenance",
 		Run: func(cmd *cobra.Command, args []string) {
 			doCheckLevelProv(checkLevelProvArgs)
 		},
--- sourcetool/cmd/prov.go
+++ sourcetool/cmd/prov.go
@@ -24,13 +24,7 @@ var (
 	provArgs ProvArgs
 	provCmd  = &cobra.Command{
 		Use:   "prov",
-		Short: "A brief description of your command",
-		Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+		Short: "Creates provenance for the given commit, but does not check policy.",
 		Run: func(cmd *cobra.Command, args []string) {
 			doProv(provArgs.prevAttPath, provArgs.commit, provArgs.prevCommit, provArgs.owner, provArgs.repo, provArgs.branch)
 		},
