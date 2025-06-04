2180b4d9646ac1e7d51aebf8d0c4b812664c47fa
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2025-06-04 15:23:36 -0700

    Add local GetNotesForCommit unit test
    This commit adds a unit test for the GetNotesForCommit func that
    connects to the github api. It will be skipped if no token is set
    in the environment.
    
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

---
+++ sourcetool/pkg/gh_control/notes_test.go
@@ -0,0 +1,44 @@
+package gh_control
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetNotesForCommit(t *testing.T) {
+	if tk := os.Getenv(tokenEnvVar); tk == "" {
+		t.Log("Skipping API test as no token is set")
+		t.Skip()
+	}
+	t.Parallel()
+	for _, tc := range []struct {
+		name        string
+		owner       string
+		repo        string
+		commit      string
+		mustBeEmpty bool
+		mustErr     bool
+	}{
+		{name: "success", owner: "slsa-framework", repo: "slsa-source-poc", commit: "e573149ab3e574abc2e5a151a04acfaf2a59b453", mustBeEmpty: false, mustErr: false},
+		{name: "non-existent", owner: "kjsdhi373iuh", repo: "lksjdhfk3773", commit: "invalid", mustBeEmpty: true, mustErr: false},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			ghc := NewGhConnection(tc.owner, tc.repo, "main")
+			notes, err := ghc.GetNotesForCommit(context.Background(), tc.commit)
+			if tc.mustErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err, tc.commit)
+			if tc.mustBeEmpty {
+				require.Empty(t, notes, "Commit: %s", tc.commit)
+			} else {
+				require.NotEmpty(t, notes, "Commit: %s", tc.commit)
+			}
+		})
+	}
+}
