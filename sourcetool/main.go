ff2facf2b7b7d6099401167a7659a6bb73a31d20
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-07-02 22:10:08 -0600

    Make cmd package internal
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- sourcetool/main.go
+++ sourcetool/main.go
@@ -4,7 +4,7 @@ Copyright © 2025 NAME HERE <EMAIL ADDRESS>
 package main
 
 import (
-	"github.com/slsa-framework/slsa-source-poc/sourcetool/cmd"
+	"github.com/slsa-framework/slsa-source-poc/sourcetool/internal/cmd"
 )
 
 func main() {
