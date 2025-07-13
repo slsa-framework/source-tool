6a9359c6b0c33bdb148bcf88563f803e683fa1be
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-07-12 20:11:06 -0600

    Remove git objects from options
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- sourcetool/pkg/sourcetool/options/options.go
+++ sourcetool/pkg/sourcetool/options/options.go
@@ -1,20 +1,12 @@
 package options
 
 import (
-	"context"
-	"errors"
 	"fmt"
 
-	"github.com/slsa-framework/slsa-source-poc/sourcetool/pkg/ghcontrol"
 	"github.com/slsa-framework/slsa-source-poc/sourcetool/pkg/policy"
 )
 
 type Options struct {
-	Repo   string
-	Owner  string
-	Branch string
-	Commit string
-
 	// Organization to look for slsa and user forks
 	UserForkOrg string
 	Enforce     bool
@@ -31,61 +23,46 @@ var Default = Options{
 	UseSSH:     true,
 }
 
-// GetGitHubConnection creates a new github connection to the repository
-// defined in the options set.
-func (o *Options) GetGitHubConnection() (*ghcontrol.GitHubConnection, error) {
-	if o.Owner == "" {
-		return nil, errors.New("owner not set")
-	}
-	if o.Repo == "" {
-		return nil, errors.New("repository not set")
-	}
+// // EnsureCommit checks the options have a commit sha defined. If not, then
+// // the latest commit from the loaded branch is read from the GitHub API.
+// func (o *Options) EnsureCommit() error {
+// 	if o.Commit != "" {
+// 		return nil
+// 	}
 
-	return ghcontrol.NewGhConnection(o.Owner, o.Repo, ghcontrol.BranchToFullRef(o.Branch)), nil
-}
+// 	if o.Branch == "" {
+// 		return errors.New("unable to fetch latest commit, no branch defined")
+// 	}
 
-// EnsureCommit checks the options have a commit sha defined. If not, then
-// the latest commit from the loaded branch is read from the GitHub API.
-func (o *Options) EnsureCommit() error {
-	if o.Commit != "" {
-		return nil
-	}
+// 	ghc, err := o.GetGitHubConnection()
+// 	if err != nil {
+// 		return fmt.Errorf("getting GitHub connection: %w", err)
+// 	}
 
-	if o.Branch == "" {
-		return errors.New("unable to fetch latest commit, no branch defined")
-	}
+// 	commit, err := ghc.GetLatestCommit(context.Background(), o.Branch)
+// 	if err != nil {
+// 		return fmt.Errorf("fetching latest commit: %w", err)
+// 	}
+// 	o.Commit = commit
+// 	return nil
+// }
 
-	ghc, err := o.GetGitHubConnection()
-	if err != nil {
-		return fmt.Errorf("getting GitHub connection: %w", err)
-	}
+// // EnsureBranch checks that the options set has a branch set and if not, it
+// // reads the repository's default branch from the GitHub API.
+// func (o *Options) EnsureBranch() error {
+// 	if o.Branch != "" {
+// 		return nil
+// 	}
 
-	commit, err := ghc.GetLatestCommit(context.Background(), o.Branch)
-	if err != nil {
-		return fmt.Errorf("fetching latest commit: %w", err)
-	}
-	o.Commit = commit
-	return nil
-}
+// 	ghc, err := o.GetGitHubConnection()
+// 	if err != nil {
+// 		return fmt.Errorf("getting GitHub connection: %w", err)
+// 	}
 
-// EnsureBranch checks that the options set has a branch set and if not, it
-// reads the repository's default branch from the GitHub API.
-func (o *Options) EnsureBranch() error {
-	if o.Branch != "" {
-		return nil
-	}
-
-	ghc, err := o.GetGitHubConnection()
-	if err != nil {
-		return fmt.Errorf("getting GitHub connection: %w", err)
-	}
-
-	branch, err := ghc.GetDefaultBranch(context.Background())
-	if err != nil {
-		return fmt.Errorf("fetching default branch: %w", err)
-	}
-	o.Branch = branch
-	return nil
-}
-
-type Fn func(*Options) error
+// 	branch, err := ghc.GetDefaultBranch(context.Background())
+// 	if err != nil {
+// 		return fmt.Errorf("fetching default branch: %w", err)
+// 	}
+// 	o.Branch = branch
+// 	return nil
+// }
