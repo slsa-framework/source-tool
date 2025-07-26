7b2df11b080e1f239e59da9dacfcfc546dcfa55e
Puerco <puerco@users.noreply.github.com>
2025-07-26 13:56:07 -0600

    Policy struct protobuf definitions (#235)
    * Add buf config files
    
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
    
    * Add policy protobuf definitions
    
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
    
    * Add buf config
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
    
    * buf generate
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
    
    * Use policy generated from proto
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
    
    * Add proto machinery and CI test
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
    
    ---------
    
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
+BOLD :=  \033[1m
+CYAN :=  \033[36m
+GREEN := \033[32m
+WHITE := \033[37m
+RESET := \033[0m
+
+.PHONY: help
+help:
+	@printf "${BOLD}${WHITE}SLSA Source Tooling Makefile Help\n=================================${RESET}\n"
+	@grep -Eh '^[a-zA-Z_-]+:.*?## .*$$' ${MAKEFILE_LIST} | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "${BOLD}${CYAN}%-25s${RESET}%s\n", $$1, $$2}'
+
 .PHONY: fakes
 fakes: ## Rebuild the implementation fakes
 	go generate ./sourcetool/...
 
+.PHONY: proto
+proto: ## Rebuild the policies and provenance predicate from protocol buffer definitions
+	buf generate
---
+++ sourcetool/pkg/policy/marshalers.go
@@ -0,0 +1,57 @@
+package policy
+
+import "encoding/json"
+
+func (branch *ProtectedBranch) MarshalJSON() ([]byte, error) {
+	type Alias ProtectedBranch
+	var since string
+	if branch.GetSince() != nil {
+		since = branch.GetSince().AsTime().Format("2006-01-02T15:04:05.000Z")
+	}
+
+	return json.Marshal(
+		&struct {
+			Since string `json:"since"`
+			*Alias
+		}{
+			Since: since,
+			Alias: (*Alias)(branch),
+		},
+	)
+}
+
+func (ctl *OrgStatusCheckControl) MarshalJSON() ([]byte, error) {
+	type Alias OrgStatusCheckControl
+	var since string
+	if ctl.GetSince() != nil {
+		since = ctl.GetSince().AsTime().Format("2006-01-02T15:04:05.000Z")
+	}
+
+	return json.Marshal(
+		&struct {
+			Since string `json:"since"`
+			*Alias
+		}{
+			Since: since,
+			Alias: (*Alias)(ctl),
+		},
+	)
+}
+
+func (tag *ProtectedTag) MarshalJSON() ([]byte, error) {
+	type Alias ProtectedTag
+	var since string
+	if tag.GetSince() != nil {
+		since = tag.GetSince().AsTime().Format("2006-01-02T15:04:05.000Z")
+	}
+
+	return json.Marshal(
+		&struct {
+			Since string `json:"since"`
+			*Alias
+		}{
+			Since: since,
+			Alias: (*Alias)(tag),
+		},
+	)
+}
