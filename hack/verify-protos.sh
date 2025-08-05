d185ce0579ac6a8a26bfcbc02899758d532ca9cd
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-08-05 15:02:27 -0600

    Run buf linter as presubmit
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- hack/verify-protos.sh
+++ hack/verify-protos.sh
@@ -9,4 +9,9 @@ set -o xtrace
 source hack/common.sh
 
 make proto
+
+# Check if the proto definitions need updating
 git diff --exit-code || exit_with_msg "Code from protocol definitions is not up to date. Please run 'make proto' and commit the result"
+
+# Check the format of the proto files
+buf lint || exit_with_msg "The proto files have linting errors. Please run 'buf lint' and fix them before committing"
